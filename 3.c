b6df01b19f547963b0879cbdbb6aa1e6ad3d5382
2semClab <158600040+2semClab@users.noreply.github.com>
2024-02-20 12:41:18 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+int n,temp;
+printf("Enter the array size: ");
+scanf("%d",&n);
+int a[n];
+printf("Enter %d elements: ",n);
+for(int i=0;i<n;i++){
+	scanf("%d",&a[i]);
+	}
+for(int i=0;i<n-1;i++){
+	for(int j=0; j<n-1-i;j++){
+		temp =0;
+		if(a[j] > a[j+1]){
+			temp =a[j];
+			a[j] = a[j+1];
+			a[j+1] =temp;
+			}
+		}
+}
+printf("Second Largest Element : %d\n",a[n-2]);
+
+return 0;
+}
